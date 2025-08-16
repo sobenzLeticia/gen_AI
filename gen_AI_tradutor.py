26c46181f1f16d48d81d2777809bfc689ec7526e
sobenzLeticia <leticiasobenz@alu.ufc.br>
2025-08-16 20:38:59 -0300

    Add files via upload

---
+++ gen_AI_tradutor.py
@@ -0,0 +1,30 @@
+from translate import Translator
+import streamlit as st
+
+def traduzir(textos, idioma):
+    tradutor = Translator(to_lang=idioma, from_lang='pt')
+
+    texto = textos
+
+    traducao = tradutor.translate(texto)
+
+    print("Seu texto traduzido: ", traducao)
+
+st.set_page_config(page_title="Tradutor Simples", layout="centered")
+
+st.title("🌍 Tradutor Simples")
+
+texto = st.text_area("Digite o texto que deseja traduzir:")
+
+idioma_destino = st.selectbox(
+    "Selecione o idioma em que deseja traduzir:",
+    ["en", "es", "fr", "de", "it", "pt"]
+)
+
+if st.button("Traduzir"):
+    if texto.strip():
+        trad = traduzir(texto, idioma_destino)
+        st.subheader("🔎 Tradução:")
+        st.success(trad)
+    else:
+        st.warning("Digite um texto para traduzir.")
